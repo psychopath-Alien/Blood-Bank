95b3f5bf99b1036051bcdfe2edb535295b86a844
psychopath-Alien <castricky28@gmail.com>
2024-12-17 15:24:42 +0800

    CRUD route for donor

--- program.py
+++ program.py
@@ -222,3 +222,147 @@ def delete_staff(id):
             "message": "Staff successfully deleted"
         }
     ), 200
+
+@app.route("/donor", methods=["GET"])
+@jwt_required()
+@role_required('admin')
+
+def get_all_donor():
+    donors = Donors.query.all()
+    return jsonify(
+        {
+            "success": True,
+            "data": [donor.to_dict() for donor in donors]
+        }
+    ), 200
+    
+
+@app.route("/donor/<int:id>", methods=['GET'])
+@jwt_required()
+@role_required('admin')
+def get_single_donor(id):
+    donors = db.session.get(Donors, id)
+    if not donors:
+        return jsonify(
+            {
+                "success": False,
+                "error": "Donors not found"
+            }
+        ), 404
+    return jsonify(
+        {
+            "success": True,
+            "data": donors.to_dict()
+        }
+    ), 200
+
+@app.route("/donor", methods=['POST'])
+@jwt_required()
+@role_required('admin', 'donor')
+def add_donor():
+    if not request.is_json:
+        return jsonify(
+            {
+                "success": False,
+                "error": "Content-type must be application/json"
+            }
+        ), 400
+
+    data = request.get_json(silent=True)
+    if not data:
+        return jsonify({"success": False, "error": "Invalid JSON"}), 400
+
+    required_fields = ["gender", "birthdate", "name", "contact", "BLOOD_BANKS_id", "ADDRESS_id", "MEDICATIONS_code", "MEDICAL_CONDITIONS_code"]
+
+    for field in required_fields:
+        if field not in data:
+            return jsonify(
+                {
+                    "success": False,
+                    "error": f"Missing field: {field}"
+                }
+            ), 400
+
+    try:
+        new_donor = Donors(
+           gender = data['gender'],
+           birthdate = data['birthdate'],
+           name = data['name'],
+           contact = data['contact'],
+           BLOOD_BANKS_id = data['BLOOD_BANKS_id'],
+           ADDRESS_id = data['ADDRESS_id'],
+           MEDICATIONS_code = data['MEDICATIONS_code'],
+           MEDICAL_CONDITIONS_code = data['MEDICAL_CONDITIONS_code']
+
+       )
+        db.session.add(new_donor)
+        db.session.commit()
+        return jsonify(
+            {
+                "success": True,
+                "data": new_donor.to_dict()
+            }
+        ), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(
+            {
+                "success": False,
+                "error": str(e)
+            }
+        ), 500
+    
+@app.route("/donor/<int:id>", methods=["PUT"])
+@jwt_required()
+@role_required('admin')
+def update_donor(id):
+    donor = db.session.get(Donors, id)
+    if not donor:
+        return jsonify(
+            {
+                "success": False,
+                "error": "Donor not found"
+            }
+        ), 404
+
+    data = request.get_json(silent=True)
+    if not data:
+        return jsonify({"success": False, "error": "Invalid JSON"}), 400
+
+    updatable_fields = ["id","gender", "birthdate", "name", "contact", "BLOOD_BANKS_id", "ADDRESS_id", "MEDICATIONS_code", "MEDICAL_CONDITIONS_code"]
+    for field in updatable_fields:
+        if field in data:
+            setattr(donor, field, data[field])
+
+    db.session.commit()
+    return jsonify(
+        {
+            "success": True,
+            "data": donor.to_dict()
+        }
+    ), 200
+
+@app.route("/donor/<int:id>", methods=["DELETE"])
+@jwt_required()
+@role_required('admin')
+def delete_donor(id):
+    donor = db.session.get(Donors, id)
+    if not donor:
+        return jsonify(
+            {
+                "success": False,
+                "error": "Donors not found"
+            }
+        ), 404
+
+    db.session.delete(donor)
+    db.session.commit()
+    return jsonify(
+        {
+            "success": True,
+            "message": "Donor successfully deleted"
+        }
+    ), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)

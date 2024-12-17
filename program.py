a6066ad32fb2ca132b88aa27142f0575e8e36be7
psychopath-Alien <castricky28@gmail.com>
2024-12-17 14:54:16 +0800

    database connection and library needed

---
+++ program.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt, verify_jwt_in_request
+from functools import wraps
+
+
+app = Flask(__name__)
+jwt = JWTManager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/bloodbank'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'sknabdoolb'
+
+db = SQLAlchemy(app)
